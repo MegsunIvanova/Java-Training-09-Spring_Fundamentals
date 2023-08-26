112fe6d0a7459164e01aab54890f49fe328ddab6
MegsunIvanova <megsunmeg@gmail.com>
2023-08-26 23:40:57 +0300

    src from training

---
+++ src/theme_03_SpringBootIntroduction/intro/src/test/java/bg/softuni/intro/IntroProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package bg.softuni.intro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
